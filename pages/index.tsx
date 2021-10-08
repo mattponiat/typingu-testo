459942013fdbb1565946e6ecc6001962b5baedd2
mattponiat <pinkyball50@gmail.com>
2021-10-08 16:14:53 +0200

    Working on buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 //Components
 import TextArea from "../components/atoms/TextArea";
 import Preview from "../components/atoms/Preview";
+import Button from "components/atoms/Button";
 
 export default function Home() {
   return (
     <>
       <Preview />
       <TextArea />
+      <Button>10</Button>
     </>
   );
 }
