448523114d9b61f05b2dbdd2bf2531dddb6d460a
mattponiat <pinkyball50@gmail.com>
2021-10-18 19:47:59 +0200

    Working random words on load, on button click

--- Hooks/useWordsAmount.ts
+++ Hooks/useWordsAmount.ts
@@ -1,21 +1,36 @@
-import { useContext } from 'react';
-import { WordsContext } from 'context/WordsContext';
+import { useContext, useEffect } from "react";
+import { WordsContext } from "context/WordsContext";
+import wordsData from "randomWords.json";
 
 export const useWordsAmount = () => {
-    const [words, setWordsState] = useContext(WordsContext);
-    const setWordsAmount = (amount: number) => {
-        if (words) {
-            switch (amount) {
-                case 10:
-                case 25:
-                case 50:
-                case 100:
-                    setWordsState(words.slice(-amount));
-                    break;
-            }
-        }
-        return;
-    };
+  const [words, setWordsState] = useContext(WordsContext);
+  const tempArray = [] as string[];
+  const lang = "english";
 
-    return { setWordsAmount };
+  const numberRandomizer = () => {
+    let randomNumber = Math.floor(Math.random() * wordsData[lang].length);
+    return randomNumber;
+  };
+
+  //Randomize words on site load
+  useEffect(() => {
+    if (words) {
+      for (let i = 0; i < wordsData[lang].length; i++) {
+        tempArray.push(wordsData[lang][numberRandomizer()]);
+      }
+    }
+    setWordsState(tempArray);
+  }, []);
+
+  const setWordsAmount = (amount: number) => {
+    if (words) {
+      for (let i = 0; i < amount; i++) {
+        tempArray.push(wordsData[lang][numberRandomizer()]);
+      }
+    }
+    setWordsState(tempArray);
+    return;
+  };
+
+  return { setWordsAmount };
 };
