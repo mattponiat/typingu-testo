8cebd580c1b0b47cedfee82df86a2a321f7de64e
mattponiat <pinkyball50@gmail.com>
2021-10-20 15:43:49 +0200

    small changes

--- components/atoms/TextArea/index.tsx
+++ components/atoms/TextArea/index.tsx
@@ -3,7 +3,12 @@ import React, { useState } from "react";
 const TextArea: React.FC = () => {
   const [text, setText] = useState("");
   return (
-    <textarea value={text} onChange={(e) => setText(e.target.value)}></textarea>
+    <div>
+      <textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
+    </div>
   );
 };
 
