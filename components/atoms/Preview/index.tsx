279aabc91497796f6d8bbd7c49468295100f88cc
mattponiat <pinkyball50@gmail.com>
2021-10-25 18:21:40 +0200

    working on words cheker

--- components/atoms/Preview/index.tsx
+++ components/atoms/Preview/index.tsx
@@ -1,15 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 //Styles
 import styled from "styled-components";
 //Context
 import { WordsContext } from "context/WordsContext";
+//Hooks
+import { useEventListener } from "hooks/useEventListener";
 
 const Preview: React.FC = () => {
   const [words] = useContext(WordsContext);
+  const [key, setKey] = useState("");
+
+  useEventListener("keydown", (e: KeyboardEvent) => {
+    setKey(e.key);
+  });
 
   const previewText = words.map((elem, i) => (
     <Text key={`${elem} ${i}`}>{elem} </Text>
   ));
+
+  const WordCheker = () => {
+    const newText = previewText
+      .map((e) => e.props.children[0])
+      .join(" ")
+      .split("");
+
+    console.log(newText);
+  };
+
+  useEffect(() => {
+    WordCheker();
+  }, [key, previewText]);
 
   return <Wrapper>{previewText}</Wrapper>;
 };
