c159f3d864af6691d64a2faa3bea145288f4f2d5
mattponiat <pinkyball50@gmail.com>
2021-12-06 17:02:35 +0100

    add context provider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 //Components
-import TypingContainer from "components/organisms/TypingContainer";
+import TypingContainer from "components-ui/organisms/TypingContainer";
+//Context
 import { WordsProvider } from "context/WordsContext";
+import { TypingProvider } from "context/store";
 //GlobalStyle
-import { GlobalStyle } from "components/globalStyle/globalStyle";
+import { GlobalStyle } from "components-ui/globalStyle/globalStyle";
 
 const Home: React.FC = () => {
   return (
     <>
       <WordsProvider>
-        <TypingContainer />
+        <TypingProvider>
+          <TypingContainer />
+        </TypingProvider>
       </WordsProvider>
       <GlobalStyle />
     </>
