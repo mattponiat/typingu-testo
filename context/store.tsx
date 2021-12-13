bb6654c953a90bf3dd194a1c2d0a185c719e0efd
mattponiat <pinkyball50@gmail.com>
2021-12-13 15:27:13 +0100

    working conterxt

--- context/store.tsx
+++ context/store.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useEffect, useState, useContext, createContext } from "react";
 //Hooks
 import { useEventListener } from "hooks/useEventListener";
 //Functions
@@ -6,21 +7,16 @@ import { WordCheker } from "functions/WordChecker";
 //Context
 import { WordsContext } from "./WordsContext";
 
-export interface Typing {
-  lettersFromWords: string[];
-  correctLetters: string[];
-  incorrectLetters: string[];
-  isActive: boolean;
-  seconds: number;
-}
+const typingCheck = () => {
+  const [lettersFromWords, setLettersFromWords] = useState([""]);
+  const [correctLetters, setCorrectLetters] = useState([""]);
+  const [incorrectLetters, setIncorrectLetters] = useState([""]);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [words] = useContext(WordsContext)!;
 
-export const typingCheck = () => {
-  const [lettersFromWords, setLettersFromWords] = React.useState([""]);
-  const [correctLetters, setCorrectLetters] = React.useState([""]);
-  const [incorrectLetters, setIncorrectLetters] = React.useState([""]);
-  const [words] = React.useContext(WordsContext)!;
   //Words initial setup
-  React.useEffect(() => {
+  useEffect(() => {
     setLettersFromWords(words.join(" ").split(""));
     setCorrectLetters([]);
     setIncorrectLetters([]);
@@ -40,22 +36,63 @@ export const typingCheck = () => {
       );
     }
   });
-  return { lettersFromWords, correctLetters, incorrectLetters };
-};
 
-const useTyping = () => {
-  const { lettersFromWords, correctLetters, incorrectLetters } = typingCheck();
+  useEffect(() => {
+    let interval: any = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    console.log(`seconds: ${seconds}`);
+    console.log(`is active: ${isActive}`);
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  useEffect(() => {
+    if (correctLetters.length > 0 || incorrectLetters.length > 0) {
+      setIsActive(true);
+    }
+    if (lettersFromWords.length === 0) {
+      setIsActive(false);
+    }
+  }, [lettersFromWords, correctLetters, incorrectLetters]);
+
   return {
     lettersFromWords,
     correctLetters,
     incorrectLetters,
+    seconds,
+    setSeconds,
+    setIsActive,
+  };
+};
+
+const useTyping = () => {
+  const {
+    lettersFromWords,
+    correctLetters,
+    incorrectLetters,
+    seconds,
+    setSeconds,
+    setIsActive,
+  } = typingCheck();
+  return {
+    lettersFromWords,
+    correctLetters,
+    incorrectLetters,
+    seconds,
+    setSeconds,
+    setIsActive,
   };
 };
 type UseTypingType = ReturnType<typeof useTyping>;
 
-const TypingContext = React.createContext<UseTypingType | null>(null);
+const TypingContext = createContext<UseTypingType | null>(null);
 
-export const useTypingContext = () => React.useContext(TypingContext)!;
+export const useTypingContext = () => useContext(TypingContext)!;
 
 export const TypingProvider = ({ children }: { children: React.ReactNode }) => (
   <TypingContext.Provider value={useTyping()}>
